e681359a38317b8428571148c50d798dc8bb1c5b
kristermyr <krister90@gmail.com>
2023-01-10 20:06:42 -0600

    changing generateMarkdown file

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,19 +1,22 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>YAY!</title>
-  </head>
-  <body>
-      
-  </body>
-  </html>`
+const generateTeam = team => {
+  const addManager = manager => {
+        return `
+<div class="card employee-card">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title">${manager.addName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.addRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.addId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.addEmail()}">${manager.addEmail()}</a></li>
+            <li class="list-group-item">Office number: ${manager.addOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+    };
   
   }
-  
-  module.exports = generateMarkdown;
+
   
