b1739afa6584bc0f1a4655dd0077facee8e01c38
kristermyr <krister90@gmail.com>
2023-01-11 12:13:31 -0600

    bcs help update

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,7 +1,7 @@
 const generateTeam = team => {
 
   // create the manager html
-  const generateManager = manager => {
+   const generateManager = manager => {
       return `
 <div class="card employee-card">
   <div class="card-header bg-primary text-white">
@@ -82,5 +82,27 @@ const generateTeam = team => {
 module.exports = team => {
 
 
+return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Engineer Team</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
+rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+<!--header-->
+<div class="container-fluid">
+<div class="row">
+<h1 class="bg-primary col-12 text-center p-5 text-white">My Team</h1>
+</div>
+${(generateTeam(team))}
+</div>
 
+</body>
+</html>
+`
 }
